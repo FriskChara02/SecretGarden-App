5429cf1fb44508cc957b97f3837adb1e15eb2efd
FriskChara02 <loi.nguyenbao02@gmail.com>
2026-08-07 16:54:04 +0700

    chore: initial Xcode project setup

---
+++ SecretGarden/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SecretGarden
+//
+//  Created by Loi Nguyen on 7/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SecretGarden/SecretGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecretGardenApp.swift
+//  SecretGarden
+//
+//  Created by Loi Nguyen on 7/8/26.
+//
+
+import SwiftUI
+
+@main
+struct SecretGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
